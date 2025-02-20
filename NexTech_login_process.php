3da70f02f1fa545ab58637cfcaeb2e6a49b57099
Joan735 <joanye777@gmail.com>
2025-02-20 10:59:10 +0100

    sixth commit

--- NexTech_login_process.php
+++ NexTech_login_process.php
@@ -11,18 +11,18 @@
     <?php
     $password = 1234;
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
-        if ($_POST["option"] == "administrator") {
-            if ($_POST["password"] == $password) {
-                header("Location: NexTech_menu_admin.html");
-            } else {
-                header("Location: NexTech_menu_noadmin.html");
-            }
+        switch ($_POST["option"]) {
+            case "administrator":
+                if ($_POST["password"] == $password) {
+                    header("Location: NexTech_menu_admin.html");
+                } else {
+                    header("Location: NexTech_menu_noadmin.html");
+                }
+                break;
+            case "user":
+                header("Location: NexTech_menu_user.html");
+                break;
         }
-        if ($_POST["option"] == "user") {
-            header("Location: NexTech_menu_user.html");
-        }
-    } else {
-        header("Location: NexTech_menu_nologin.html");
     }
     ?>
 </body>
