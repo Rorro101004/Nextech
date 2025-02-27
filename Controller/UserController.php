dc4e9cfb13ef3a57572c5a5ab79b7efb98d3888e
Joan735 <joanye777@gmail.com>
2025-02-27 09:57:19 +0100

    commit

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -14,13 +14,13 @@
         switch ($_POST["option"]) {
             case "administrator":
                 if ($_POST["password"] == $password) {
-                    header("Location: ../NexTech_menu/NexTech_menu_admin.html");
+                    header("Location: ../View/NexTech_menu_admin.html");
                 } else {
-                    header("Location: ../NexTech_menu/NexTech_menu_noadmin.html");
+                    header("Location: ../View/NexTech_menu_noadmin.html");
                 }
                 break;
             case "user":
-                header("Location: ../NexTech_menu/NexTech_menu_user.html");
+                header("Location: ../View/NexTech_menu_user.html");
                 break;
         }
     }
