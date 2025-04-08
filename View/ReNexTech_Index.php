ffd74e1d5bc85d500f7f4ffbe296886761042b01
Takkun <rodrimd231@gmail.com>
2025-04-08 02:02:00 +0200

    Improved mobile layout and responsive navbar #2

--- View/ReNexTech_Index.php
+++ View/ReNexTech_Index.php
@@ -1,11 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="NexTech.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css" />
+    <link rel="stylesheet" href="NexTech.css" />
     <title>Document</title>
 </head>
-<body>
-    hello
+
+<body class="figma-Background">
+    <nav>
+        <input type="checkbox" id="sidebar-active">
+        <label for="sidebar-active" class="open-sidebar-button">
+            <svg width="32" height="32" viewBox="0 0 34 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 1.77083C0 0.792827 0.792827 0 1.77083 0H32.2291C33.2071 0 33.9999 0.792827 33.9999 1.77083C33.9999 2.74883 33.2071 3.54166 32.2291 3.54166H1.77083C0.792828 3.54166 0 2.74883 0 1.77083ZM0 8.85414C0 7.87614 0.792827 7.08331 1.77083 7.08331H32.2291C33.2071 7.08331 33.9999 7.87614 33.9999 8.85414C33.9999 9.83214 33.2071 10.625 32.2291 10.625H1.77083C0.792828 10.625 0 9.83214 0 8.85414ZM0 15.9375C0 14.9595 0.792827 14.1666 1.77083 14.1666H32.2291C33.2071 14.1666 33.9999 14.9595 33.9999 15.9375C33.9999 16.9155 33.2071 17.7083 32.2291 17.7083H1.77083C0.792828 17.7083 0 16.9155 0 15.9375ZM0 23.0208C0 22.0428 0.792827 21.2499 1.77083 21.2499H32.2291C33.2071 21.2499 33.9999 22.0428 33.9999 23.0208C33.9999 23.9988 33.2071 24.7916 32.2291 24.7916H1.77083C0.792828 24.7916 0 23.9988 0 23.0208Z" fill="#485C6F" />
+            </svg>
+        </label>
+
+        <label id="overlay" for="sidebar-active">
+
+        </label>
+        <div class="links-container">
+            <label for="sidebar-active" class="close-sidebar-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                </svg>
+            </label>
+        <!-- Replace # with the .php files -->
+            <a class="home-link" href="#">Home</a>
+            <a href="#">Events</a>
+            <a href="#">About Us</a>
+
+        </div>
+        <div class="Login-Register">
+            <a href="#">Register</a>
+            <a href="#">Login</a>
+        </div>
+
+    </nav>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea unde optio ipsam. Quibusdam, modi quaerat.
+    <footer>
+        <div class="footer-content"> 
+            <h3>TestTitulo</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa expedita autem veritatis adipisci nemo quis!</p>
+        </div>
+    </footer>
 </body>
-</html>>
+
+</html>
