61cc5313a654325c5e621b78fcb44d4afe89facb
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-04-10 22:25:48 +0200

    header fixed

--- View/Nextech_about_us.php
+++ View/Nextech_about_us.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION["logged"])) {
+    $_SESSION["logged"] = false; // Establece un valor predeterminado si no está definido
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,51 +14,51 @@
   <link rel="stylesheet" href="NexTech.css">
 </head>
 <body>
-  <header>
-    <div class="header">
-      <div class="start">
-        <div class="images">
-          <div class="logo">
-            <a href="NexTech_index.php"><img src="Images/NexTech logo.png" alt="Image Logo NexTech"
-                width="100px"></a>
-          </div>
-        </div>
-        <div class="options">
-          <div class="events">
-            <a href="NexTech_events.php" style="color:rgb(94, 6, 130)"><b>Events</b></a>
-          </div>
-          <?php if ($_SESSION["logged"] == false) { ?>
-            <div class="profile_no_logged">
-              <p style="color: rgb(147, 0, 233)">Profile</p>
+<header>
+        <div class="header">
+            <div class="start">
+                <div class="images">
+                    <div class="logo">
+                        <a href="NexTech_index.php"><img src="Images/NexTech logo.png" alt="Image Logo NexTech"
+                                width="100px"></a>
+                    </div>
+                </div>
+                <div class="options">
+                    <div class="events">
+                        <a href="NexTech_events.php" style="color:rgb(94, 6, 130)"><b>Events</b></a>
+                    </div>
+                    <?php if ($_SESSION["logged"] == false) { ?>
+                        <div class="profile_no_logged">
+                            <p style="color: rgb(147, 0, 233)">Profile</p>
+                        </div>
+                    <?php } else if ($_SESSION["logged"] == true) { ?>
+                        <div class="profile_logged">
+                            <a href="NexTech_profile.php" style="color:rgb(147, 0, 233)">Profile</a>
+                        </div>
+                    <?php } ?>
+                    <div class="info">
+                        <p style="color:rgb(147, 0, 233)">Info</p>
+                    </div>
+                </div>
             </div>
-          <?php } else if ($_SESSION["logged"] == true) { ?>
-            <div class="profile_logged">
-              <a href="NexTech_profile.php" style="color:rgb(147, 0, 233)">Profile</a>
-            </div>
-          <?php } ?>
-          <div class="info">
-            <p style="color:rgb(147, 0, 233)">Info</p>
-          </div>
+            <?php if ($_SESSION["logged"] == false) { ?>
+                <div class="login_register">
+                    <div class="register">
+                        <a href="NexTech_register.php">Register</a>
+                    </div>
+                    <div class="login">
+                        <a href="NexTech_login.php">Login</a>
+                    </div>
+                </div>
+            <?php } else if ($_SESSION["logged"] == true) { ?>
+                <div class="logout">
+                    <form action="../Controller/UserController.php" method="post">
+                        <input type="submit" name="logout" value="Log out">
+                    </form>
+                </div>
+            <?php } ?>
         </div>
-      </div>
-      <?php if ($_SESSION["logged"] == false) { ?>
-        <div class="login_register">
-          <div class="register">
-            <a href="NexTech_register.php">Register</a>
-          </div>
-          <div class="login">
-            <a href="NexTech_login.php">Login</a>
-          </div>
-        </div>
-      <?php } else if ($_SESSION["logged"] == true) { ?>
-        <div class="logout">
-          <form action="../Controller/UserController.php" method="post">
-            <input type="submit" name="logout" value="Log out">
-          </form>
-        </div>
-      <?php } ?>
-    </div>
-  </header>
+    </header>
   <section>
     <div class="section">
       <h2>About Us</h2>
@@ -77,7 +83,14 @@
       <p>Looking for the sonnest big event in tech ? <strong>[Company Name]</strong> is here to keep you informed and connected. Start exploring today!</p>
     </div>
   </section>
-
+  <footer>
+        <div class="contact-info">
+            <p>&copy; 2025 NexTech.com. All rights reserved.</p>
+            <p>Address: Calle Pelai 123, Barcelona, Spain</p>
+            <p>Phone number: +123 456 789</p>
+            <p>Email: infonextech@gmail.com</p>
+        </div>
+    </footer>
 </body>
 
 </html>
